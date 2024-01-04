e0d5e66a6c5eb3bbac4b993b1538b25661c77f3a
chhavi Goel <Chhavi.Goel@ratesetter.com>
2024-01-04 13:41:25 +0000

    Added CustomAttribute and Middleware for api key authentication

---
+++ ApiAuthentication/Attributes/ApiKeyAttribute.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace APIAuthentication.Attributes
+{
+    [AttributeUsage(validOn: AttributeTargets.Class | AttributeTargets.Method)]
+    public class ApiKeyAttribute : Attribute, IAsyncActionFilter
+    {
+        private const string ApiKeyName = "ApiKey";
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            if (!context.HttpContext.Request.Headers.TryGetValue(ApiKeyName, out var extractedApiKey))
+            {
+                context.Result = new ContentResult()
+                {
+                    StatusCode = 401,
+                    Content = "API key was not provided"
+                };
+
+                return;
+            }
+
+            var appSettings = context.HttpContext.RequestServices.GetRequiredService<IConfiguration>();
+            var apiKey = appSettings.GetValue<string>(ApiKeyName);
+            if (apiKey == null || apiKey.Equals(extractedApiKey))
+            {
+                context.Result = new ContentResult()
+                {
+                    StatusCode = 401,
+                    Content = "API key is not valid"
+                };
+
+                return;
+            }
+
+            await next();
+        }
+    }
+}
